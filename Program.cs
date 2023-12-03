d943e267811679b44d38ea6c5ad736783d3a30bb
anang shaleh bakti <anang.bakti@gmail.com>
2023-12-03 14:29:40 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<string> pets = new List<string>();
+
+Accept("Kangaroo");
+Accept("Bunny");
+Console.WriteLine(HealingPets() + " Healed");
+Console.WriteLine(HealingPets() + " Healed");
+
+string HealingPets() {
+    string healedPets = pets[0];    
+    pets.RemoveAt(0);
+    return healedPets;
+}
+
+void Accept(string petsName) { 
+    pets.Add(petsName);
+    Console.WriteLine(petsName + " Added");
+}
